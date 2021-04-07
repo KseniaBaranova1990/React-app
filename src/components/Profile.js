1321b18f2ec4f5c287b69b40bb836b08ccfa1c78
Ksenia Baranova <baranovaksenia1990@gmail.com>
2021-04-07 18:58:51 +0300

    header nav main

---
+++ src/components/Profile.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <div>
+        <img src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg" />
+      </div>
+      <div>Avatar+/Description</div>
+      <div>My post</div>
+      <div>New post</div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
