71207a47c29e96a321f00e4b4725667136c999d5
Ksenia Baranova <baranovaksenia1990@gmail.com>
2021-04-08 13:16:53 +0300

    modules.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
+import s from "./Header.module.css";
 
 const Header = () => {
   return (
-  <header className="header">
-    <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" />
-  </header>
-  )
+    <header className={s.header}>
+      <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" />
+    </header>
+  );
 };
-
 
 export default Header;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,15 @@
 import React from "react";
+import s from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className="profile">
+    <div className={s.content}>
       <div>
         <img src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg" />
       </div>
-      <div>Avatar+/Description</div>
-      <div>My post</div>
-      <div>New post</div>
+      <div className={s.item}>Avatar+/Description</div>
+      <div className={s.item}>My post</div>
+      <div className={s.item}>New post</div>
       <div>
         <div>Post 1</div>
         <div>Post 2</div>
